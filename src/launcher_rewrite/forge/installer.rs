be1ec4497a4264e6a012ed36cb70a1603ae7b90d
kpzip <kpzipgames@gmail.com>
2024-12-21 02:25:10 -0800

    forge support

--- src/launcher_rewrite/forge/installer.rs
+++ src/launcher_rewrite/forge/installer.rs
@@ -1,11 +1,16 @@
 use std::fmt::format;
+use std::{fs, io};
+use std::fs::File;
 use std::num::NonZeroU64;
 use std::path::{Path, PathBuf};
+use std::process::Command;
+use const_format::concatcp;
 use iced::widget::markdown::Url;
 use crate::launcher_rewrite::jar_utils::extractor::extract_if_needed;
 use crate::launcher_rewrite::installer::Downloadable;
+use crate::launcher_rewrite::manifest::GAME_VERSION_MANIFEST;
 use crate::launcher_rewrite::mod_loader_version_manifest::ModLoaderVersionInfo;
-use crate::launcher_rewrite::path_handler::{get_bin_path, get_vanilla_client_json_path, temp_file_path};
+use crate::launcher_rewrite::path_handler::{DUMMY_INSTALL_DIR_NAME, DUMMY_LAUNCHER_PROFILES_JSON_NAME, get_bin_path, get_vanilla_client_json_path, PATH_SEP, temp_file_path};
 use crate::launcher_rewrite::profiles::ModLoader;
 use crate::launcher_rewrite::util::hash::FileHash;
 
@@ -37,14 +42,28 @@ impl Downloadable for ForgeJarDownloadable<'_> {
 pub fn download(loader_info: &ModLoaderVersionInfo, game_version: &str) {
 
     let loader_version = loader_info.version_name();
+    let game_version = GAME_VERSION_MANIFEST.sanitize_version_name(game_version);
 
     // Download installer jar and extract version json and version jar
 
     // Paths
+    let client_jar_name = format!("forge-{}-{}-client.jar", game_version, loader_version);
     let temp_path = temp_file_path(format!("forge-{}-{}.jar.tmp", game_version, loader_version).as_str());
+    let dummy_profiles_json_path = temp_file_path(concatcp!(DUMMY_INSTALL_DIR_NAME, PATH_SEP, DUMMY_LAUNCHER_PROFILES_JSON_NAME));
+    let install_dir = dummy_profiles_json_path.parent().expect("Bruh");
     let forge_client_path = {
         let mut p = get_bin_path(game_version);
-        p.push(format!("forge-{}-{}-client.jar", game_version, loader_version));
+        p.push(client_jar_name.as_str());
+        p
+    };
+    let generated_client_path = {
+        let mut p = install_dir.to_owned();
+        p.push("libraries");
+        p.push("net");
+        p.push("minecraftforge");
+        p.push("forge");
+        p.push(format!("{}-{}", game_version, loader_version));
+        p.push(client_jar_name.as_str());
         p
     };
     let forge_shim_path = {
@@ -52,6 +71,7 @@ pub fn download(loader_info: &ModLoaderVersionInfo, game_version: &str) {
         p.push(format!("forge-{}-{}-shim.jar", game_version, loader_version));
         p
     };
+    if let Some(p) = temp_path.parent() { if let Err(e) = fs::create_dir_all(p) { eprintln!("Error creating directory! {}", e) } };
 
     let client_json_internal_path = Path::new(CLIENT_JSON_INTERNAL_PATH);
     let client_json_external_path = get_vanilla_client_json_path(game_version, ModLoader::Forge, loader_info.version_name());
@@ -65,4 +85,13 @@ pub fn download(loader_info: &ModLoaderVersionInfo, game_version: &str) {
 
     // Patch forge client jar
     // TODO
+
+    // Just run the forge installer so that way we don't have to patch the jar ourselves (For now)
+    // Fake it till you make it. -Kyle Schmerge 2024
+    fs::write(dummy_profiles_json_path.as_path(), []).expect("failed to write dummy json.");
+    let _ = Command::new("java").current_dir(temp_path.as_path()).args(["-jar", temp_path.to_string_lossy().as_ref(), "--installClient", install_dir.to_string_lossy().as_ref()]).output();
+    //println!("path: {:?}", generated_client_path.as_path());
+    let mut generated = File::open(generated_client_path.as_path()).expect("Failed to open generated file");
+    let mut bin_file = File::create(forge_client_path.as_path()).expect("Failed to open output file");
+    io::copy(&mut generated, &mut bin_file).expect("Failed to copy");
 }
