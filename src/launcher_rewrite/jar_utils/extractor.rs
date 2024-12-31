afbdd464e99ccbdb6debce60d460cca82a2d93d0
kpzip <kpzipgames@gmail.com>
2024-12-31 00:24:26 -0800

    error handling

--- src/launcher_rewrite/jar_utils/extractor.rs
+++ src/launcher_rewrite/jar_utils/extractor.rs
@@ -1,5 +1,6 @@
 use std::{fs, io};
 use std::path::{Path, PathBuf};
+use crate::launcher_rewrite::error::LauncherError;
 
 #[cfg(all(target_os = "windows", target_arch = "x86_64"))]
 pub const NATIVE_JARS_SUFFIX: &str = "natives-windows.jar";
@@ -14,41 +15,44 @@ pub const NATIVE_JARS_SUFFIX: &str = "natives-macos-arm64.jar";
 #[cfg(target_os = "linux")]
 pub const NATIVE_JARS_SUFFIX: &str = "natives-linux.jar";
 
-pub fn extract_if_needed(extracted_path: &Path, internal_path: &Path, jar_path: &Path) {
-    fs::create_dir_all(extracted_path.parent().unwrap()).unwrap();
+pub fn extract_if_needed(extracted_path: &Path, internal_path: &Path, jar_path: &Path) -> Result<(), LauncherError> {
+    fs::create_dir_all(extracted_path.parent().unwrap())?;
 
-    let jarfile = fs::File::open(jar_path).unwrap();
-    let mut archive = zip::ZipArchive::new(jarfile).unwrap();
-    let internal_name = internal_path.as_os_str().to_str().unwrap();
+    let jar_file = fs::File::open(jar_path)?;
+    let mut archive = zip::ZipArchive::new(jar_file)?;
+    let internal_name = internal_path.as_os_str().to_string_lossy();
     // println!("Extracting: {}", internal_name);
-    let mut internal_file = match archive.by_name(internal_name) {
+    let mut internal_file = match archive.by_name(internal_name.as_ref()) {
         Ok(s) => s,
         Err(e) => {
-            eprintln!("Could not open internal file {}, because of the error: {}", internal_name, e);
-            return;
+            eprintln!("Could not open internal file {}, because of the error: {}", internal_name.as_ref(), e);
+            return e.into();
         }
     };
-    let mut write_file = fs::File::create(extracted_path).unwrap();
-    io::copy(&mut internal_file, &mut write_file).expect("TODO: panic message");
+    let mut write_file = fs::File::create(extracted_path)?;
+    io::copy(&mut internal_file, &mut write_file)?;
+    Ok(())
 }
 
-pub fn extract_dlls_from_jar(bin_path: &Path, jar_path: &Path) {
+pub fn extract_dlls_from_jar(bin_path: &Path, jar_path: &Path) -> Result<(), LauncherError> {
     // println!("Jar Path: {}", jar_path.display());
-    let jar_file = fs::File::open(jar_path).unwrap();
-    let mut archive = zip::ZipArchive::new(jar_file).unwrap();
+    let jar_file = fs::File::open(jar_path)?;
+    let mut archive = zip::ZipArchive::new(jar_file)?;
     let mut extracted_file_path: PathBuf = bin_path.into();
 
     let dll_names: Vec<String> = archive.file_names().filter(|n| n.ends_with(".dll")).map(String::from).collect();
-    dll_names.iter().for_each(|internal_name| {
+    dll_names.iter().map(|internal_name| {
         let external_name = match internal_name.rsplit_once('/') {
             None => internal_name,
             Some((_, rhs)) => rhs,
         };
         extracted_file_path.push(external_name);
         //println!("Extracting dll file: {} to {}", internal_name, extracted_file_path.as_os_str().to_str().unwrap());
-        let mut internal_file = archive.by_name(internal_name).unwrap();
-        let mut external_file = fs::File::create(&extracted_file_path).unwrap();
-        io::copy(&mut internal_file, &mut external_file).expect("TODO: panic message");
+        let mut internal_file = archive.by_name(internal_name)?;
+        let mut external_file = fs::File::create(&extracted_file_path)?;
+        io::copy(&mut internal_file, &mut external_file)?;
         extracted_file_path.pop();
-    });
+        Ok(())
+    }).collect::<Result<Vec<()>, LauncherError>>()?;
+    Ok(())
 }
