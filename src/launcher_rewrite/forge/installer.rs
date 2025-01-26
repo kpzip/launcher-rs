538b6d33d9e55d63831fc9d8c43ec0b27e15eb17
kpzip <kpzipgames@gmail.com>
2025-01-26 15:38:13 -0800

    add mod loader to sanitize function args

--- src/launcher_rewrite/forge/installer.rs
+++ src/launcher_rewrite/forge/installer.rs
@@ -43,7 +43,7 @@ impl Downloadable for ForgeJarDownloadable<'_> {
 pub fn download(loader_info: &ModLoaderVersionInfo, game_version: &str) -> Result<(), LauncherError> {
 
     let loader_version = loader_info.version_name();
-    let game_version = GAME_VERSION_MANIFEST.sanitize_version_name(game_version);
+    let game_version = GAME_VERSION_MANIFEST.sanitize_version_name(game_version, ModLoader::Forge);
 
     // Download installer jar and extract version json and version jar
 
