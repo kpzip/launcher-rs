f89c773b08c415066bfee6a3ea2dcb2654186f58
kpzip <kpzipgames@gmail.com>
2025-01-26 15:39:20 -0800

    make sanitized game version its own struct

--- src/launcher_rewrite/game_version.rs
+++ src/launcher_rewrite/game_version.rs
@@ -1,19 +1,15 @@
 use crate::launcher_rewrite::manifest::GAME_VERSION_MANIFEST;
 
-pub struct GameVersion<'a> {
-    unsanitized: &'a str,
+pub struct SanitizedGameVersion<'a> {
+    sanitized: &'a str,
 }
 
-impl<'a> GameVersion<'a> {
+impl<'a> SanitizedGameVersion<'a> {
 
     pub fn from_str(game_version: &'a str) -> Self {
         Self {
-            unsanitized: game_version,
+            sanitized: game_version,
         }
-    }
-
-    pub fn as_str(&self) -> &str {
-        GAME_VERSION_MANIFEST.sanitize_version_name(self.unsanitized)
     }
 
 }
